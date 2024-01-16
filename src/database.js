eb9c3d4da12b78877b24049ecccfece3e5babc71
kaki <kakivi2000@gmail.com>
2024-01-16 12:12:35 +0100

    sisiis

--- src/database.js
+++ src/database.js
@@ -8,7 +8,8 @@ const pool = createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: process.env.PORT
 });
 
 export default pool;
